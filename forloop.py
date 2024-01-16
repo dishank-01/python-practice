4cd004de7b6da3abe9dc45c8ecc71ee376729e60
dishank-01 <156780182+dishank-01@users.noreply.github.com>
2024-01-16 14:20:09 +0530

    commit

---
+++ forloop.py
@@ -0,0 +1,24 @@
+"""for i in range(1,11,1):
+    print(i)
+    
+for b in range(10,0,-1):
+    print(b)"""
+"""   
+n=int(input("enter number"))
+for i in range(2,21,1):
+    r=n%i if r==0:
+        print("number is not prime")
+        break
+    else:
+        print("number is prime")
+        break
+    if r==0:
+            print(n)
+            n=n+1
+            
+    else:
+        print("not prime")"""
+        
+        
+    
+    
