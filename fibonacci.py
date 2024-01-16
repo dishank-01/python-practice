908d0c734d0c4e651ce2c1488b6e821e160c177c
dishank-01 <156780182+dishank-01@users.noreply.github.com>
2024-01-16 14:18:47 +0530

    fibonacci sieries program

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+#fibonacci
+
+n=int(input("enter number"))
+a=0
+b=1
+sum=0
+k=1
+"""if n<=0:
+    print("please enter valid number")
+else:
+    for i in range(0,n):
+        print (sum,end="")
+        a=b
+        b=sum
+        sum=a+b
+  #with while loop      
+while k<=n-2:
+    print(sum,end=" ")
+    a=b
+    b=sum
+    sum=a+b
+    k=k+1"""
+    
+        
